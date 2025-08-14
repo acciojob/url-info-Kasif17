8eb9e5c48dc5040af4d6d0dddf43212e93e2dd4c
Mohd Kasif Khan <developer786kasif@gmail.com>
2025-08-14 16:50:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 //your JS code here. If required.
-function showUrlLength() {
-   
-    let userUrl = prompt("Enter your URL:");
+function UrlLength(){
+	 let currentUrl = location.href;
 
-    if (userUrl) {
-        let urlLength = userUrl.length;
-        alert("The length of the URL is: " + urlLength);
-    } else {
-        alert("No URL entered!");
-    }
+    let urlLength = currentUrl.length;
+	return urlLength
 }
-
-showUrlLength();
+const input = prompt("Enter a string");
+alert(UrlLength(input)); 
